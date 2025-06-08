f283e507f99b3eba0c965906a9684fd0cff32730
Fitiavana Sambatra <Ghostrex2@gmail.com>
2025-06-08 21:09:42 +0300

    feat : companions card

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import { Button } from '@/components/ui/button'
+import CompanionCard from '@/components/companion-card'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div><Button>Hello</Button></div>
+    <main>
+      <h1 className="text-2xl underline">
+        Popular Companions
+      </h1>
+      <section className='home-section'>
+        <CompanionCard />
+        <CompanionCard />
+        <CompanionCard />
+      </section>
+    </main>
   )
 }
 
